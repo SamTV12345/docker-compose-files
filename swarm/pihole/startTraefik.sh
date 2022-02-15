181fec3819e180a4ef2148bf2ccdb629a06eab87
Samuel <samuel1998.schwanzer@gmail.com>
2022-02-15 20:21:39 +0100

    Updated to run traefik on port 80

--- swarm/pihole/startTraefik.sh
+++ swarm/pihole/startTraefik.sh
@@ -3,6 +3,6 @@
 # // @formatter:off
 sudo docker service update --label-add "traefik.docker.network=pihole-stacked_default" --label-add "traefik.port=80" --label-add "traefik.frontend.rule=PathPrefix:/" --label-add "traefik.backend.loadbalancer.stickiness=true" pihole-stacked_pihole
 
-sudo docker service create --name traefik -p8080:80 -p9090:8080 --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock --mode=global --constraint 'node.role == manager' --network pihole-stacked_default traefik:v1.7.30 --docker --docker.swarmmode --docker.watch --web --loglevel=DEBUG
+sudo docker service create --name traefik -p80:80 -p9090:8080 --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock --mode=global --constraint 'node.role == manager' --network pihole-stacked_default traefik:v1.7.30 --docker --docker.swarmmode --docker.watch --web --loglevel=DEBUG
 
 # See https://www.vultr.com/de/docs/sticky-session-with-docker-swarm-ce-on-debian-9
