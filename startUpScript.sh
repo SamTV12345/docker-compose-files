e96222ad8746e8e8bf3a02cb2c4f95ad95b6ba51
Samuel <samuel1998.schwanzer@gmail.com>
2022-07-07 18:32:09 +0200

    Added question for overriding .env file.

--- startUpScript.sh
+++ startUpScript.sh
@@ -7,6 +7,19 @@ choice=$(whiptail --title "Choose the service to deploy" --radiolist \
   "Choose one?" 20 70 5 \
   $VAULTWARDEN "Vaultwarden " OFF 3>&1 1>&2 2>&3)
 
+if test -f "$PWD/.env"
+then
+  echo "Do you want to override the existing .env file? (y/n)"
+  read -r
+  if [ "$REPLY" = "y" ]
+  then
+    echo "Overriding .env file"
+  else
+    echo "Stopping process..."
+    exit 0
+fi
+fi
+
 queryUrl=''
 service=''
 filename='docker-'
@@ -17,26 +30,26 @@ if [ "$choice" = $VAULTWARDEN ]; then
   echo "Signups allowed(y/n)?"
   read -r
   if [[ ${REPLY} = 'y' ]]; then
-    echo "SIGNUP_ALLOWED=true" >"vaultwarden.env"
+    echo "SIGNUP_ALLOWED=true" >".env"
   else
-    echo "SIGNUP_ALLOWED=false" >"vaultwarden.env"
+    echo "SIGNUP_ALLOWED=false" >".env"
   fi
 
   echo "Disable Admin console?"
   read -r
   if [[ ${REPLY} = 'y' ]]; then
-    echo "DISABLE_ADMIN_TOKEN=true" >>"vaultwarden.env"
+    echo "DISABLE_ADMIN_TOKEN=true" >>".env"
   else
-    echo "DISABLE_ADMIN_TOKEN=false" >>"vaultwarden.env"
+    echo "DISABLE_ADMIN_TOKEN=false" >>".env"
   fi
 
   echo "Enter port for vaultwarden?"
   read -r
-  echo "PORT=${REPLY}">> "vaultwarden.env"
+  echo "PORT=${REPLY}">> ".env"
 
   echo "Enter Volume mount?"
   read -r
-  echo "VOLUME_MOUNT=${REPLY}">> "vaultwarden.env"
+  echo "VOLUME_MOUNT=${REPLY}">> ".env"
 
 fi
 
